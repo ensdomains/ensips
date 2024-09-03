d1dc3e10fbc35211aee710d6723c8b84899e2f22
Luc <luc@lucemans.nl>
2024-09-03 19:52:32 +0000

    Introduce frontmatter parsing

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,71 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 import { App } from './src/App';
-import { readFile, rmdir, mkdir, writeFile, readdir } from 'fs/promises';
+import { readFile, mkdir, writeFile, readdir, rm } from 'fs/promises';
 import { remark } from 'remark';
 import html from 'remark-html';
+import remarkFrontMatter from 'remark-frontmatter';
+import { parse as parseYaml, YAMLParseError } from 'yaml';
+
+export type Frontmatter = {
+    description: string;
+    contributors: string[];
+    ensip: {
+        status: string;
+        created: string;
+    }
+}
+
+console.log('Building preview...');
 
 let markdown_files = await readdir('../ensips');
 const files = markdown_files.map(x => `../ensips/${x}`);
 
 // delete the dist folder
 try {
-    await rmdir('./dist', { recursive: true })
+    await rm('./dist', { recursive: true })
 } catch (e) {
-    console.log(e);
+    // Soft error if directory doesn't exist
+    // console.log(e);
 }
 
 await mkdir('./dist');
 
 for (const file of files) {
-    const fileData = await readFile(file, 'utf8');
+    try {
+        const fileData = await readFile(file, 'utf8');
 
-    const result = await remark().use(html).process(fileData);
+        let frontmatter: string = '';
 
-    const x = renderToStaticMarkup(<App markdown={result.value.toString()} />);
+        const result = await remark().use(remarkFrontMatter).use(html).use(function () {
+            return function (tree) {
+                // console.dir(tree)
+                let first = ((tree as any)['children'] as { type: string, value?: string }[]).shift();
+                if (first && first.type === 'yaml' && first.value) {
+                    frontmatter = first.value;
+                } else {
+                    throw new Error('No frontmatter found');
+                }
+            }
+        }).process(fileData);
 
-    // write to file
-    await writeFile(`./dist/${file.split('/').pop()?.replace('.md', '.html')}`, x);
+        const frontmatter_parsed = parseYaml(frontmatter) as Frontmatter;
+        console.log(frontmatter_parsed);
+
+        const x = renderToStaticMarkup(<App markdown={result.value.toString()} frontmatter={frontmatter_parsed} />);
+
+        // write to file
+        await writeFile(`./dist/${file.split('/').pop()?.replace('.md', '.html')}`, x);
+    } catch (error) {
+        const directPath = file.replace('../', '/app/');
+
+        if (error instanceof YAMLParseError) {
+            console.log(error.linePos);
+            // console.log('::error file=' + directPath + ',line=' + error.mark.line + ',col=' + error.mark.column + ',endColumn=' + error.mark.column + '::Unable to load file')
+        }
+
+        console.log(error);
+        console.log('::error file=' + directPath + ',line=1,col=1,endColumn=2::Unable to load file')
+    }
 }
 
 const static_files = ['./public/index.css'];
@@ -35,3 +76,5 @@ for (const file of static_files) {
     // write to file
     await writeFile(`./dist/${file.split('/').pop()}`, fileData);
 }
+
+console.log('Preview built successfully!');
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
+import type { Frontmatter } from '..';
 import { Navbar } from './Navbar';
 
-export const App: React.FC<{ markdown: string }> = ({ markdown }) => {
+export const App: React.FC<{ markdown: string, frontmatter: Frontmatter }> = ({ markdown, frontmatter }) => {
     return (
         <html>
             <head>
@@ -10,6 +11,42 @@ export const App: React.FC<{ markdown: string }> = ({ markdown }) => {
             <body>
                 <article>
                     <Navbar />
+                    <div className="front space-y-4">
+                        <div>
+                            <b>
+                                Description
+                            </b>
+                            <div>
+                                {frontmatter.description}
+                            </div>
+                        </div>
+                        <div>
+                            <b>
+                                Status
+                            </b>
+                            <div>
+                                {frontmatter.ensip.status}
+                            </div>
+                        </div>
+                        <div>
+                            <b>
+                                Created
+                            </b>
+                            <div>
+                                {frontmatter.ensip.created}
+                            </div>
+                        </div>
+                        <div>
+                            <b>Author{frontmatter.contributors.length > 1 ? 's' : ''}</b>
+                            <ul>
+                                {
+                                    frontmatter.contributors.map((contributor, index) => {
+                                        return <li key={index}>{contributor}</li>
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
                     <div dangerouslySetInnerHTML={{ __html: markdown }} />
                     <div>
                         ENSIPs
